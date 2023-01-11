47432e3b3f86e599f3aa149ac82a7bd69275e7d0
Isabela Chamorra Modesto <isabelam@ciandt.com>
2023-01-11 10:31:08 -0300

    recreating project

---
+++ HeroApp2/Model/HeroModel.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+/*class Hero : Codable{
+    var name: String
+    var description: String
+    
+    init(name: String, description: String){
+        self.name = name
+        self.description = description
+    }
+}*/
+
+struct Hero : Codable {
+    let name : String
+    var description : String
+    let thumbnail : Thumbnail
+}
